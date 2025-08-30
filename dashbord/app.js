aa3c386c00311be7e5897a19e31a1ca8fcbb8726
Abdulla012 <Abdulla1xailany2@gmail.com>
2025-08-30 09:02:01 +0300

    Add files via upload

---
+++ dashbord/app.js
@@ -0,0 +1,32 @@
+const hamburger = document.querySelector(".toggle-btn")
+const toggle = document.querySelector("#icon")
+hamburger.addEventListener("click", function(){
+    document.querySelector("#sidebar").classList.toggle("expand")
+    toggle.classList.toggle("fa-circle-chevron-right")
+    toggle.classList.toggle("fa-circle-chevron-left")
+})
+
+// chart bar example
+new Chart(document.getElementById("bar-chart-grouped"), {
+    type: 'bar',
+    data: {
+      labels: ["1900", "1950", "1999", "2050"],
+      datasets: [
+        {
+          label: "Africa",
+          backgroundColor: "#3e95cd",
+          data: [133,221,783,2478]
+        }, {
+          label: "Europe",
+          backgroundColor: "#8e5ea2",
+          data: [408,547,675,734]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Population growth (millions)'
+      }
+    }
+});
